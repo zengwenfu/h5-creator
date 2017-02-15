d5ca6507a17e31476ae19f092c29d054b25d9e7a
曾文富 <zengwenfu836@pingan.com.cn>
2017-02-15 23:25:01 +0800

    修改首页为index索引

--- server/routers/build.js
+++ server/routers/build.js
@@ -120,7 +120,7 @@ module.exports = function(app) {
     /**
      *  主页
      */
-    app.get('/home', function(req, res) {
+    app.get('/index', function(req, res) {
         var domain = process.host;
         actDao.findList(function(data) {
             res.render('home.html', {
